5f7f10a322ee20d9ddf4c8c6f413cf65927b6143
irvento <irvenabarquez@gmail.com>
2025-02-14 08:42:41 +0800

    lower cased routes

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -55,11 +55,11 @@ import About from './About';
                 </IonMenu>
                 
                 <IonRouterOutlet id="main">
-                    <Route exact path="/it35-lab/app/Home" component={Home} />
-                    <Route exact path="/it35-lab/app/About" component={About} />
+                    <Route exact path="/it35-lab/app/home" component={Home} />
+                    <Route exact path="/it35-lab/app/about" component={About} />
 
                     <Route exact path="/it35-lab/app">
-                        <Redirect to="/it35-lab/app/Home"/>
+                        <Redirect to="/it35-lab/app/home"/>
                     </Route>
                 </IonRouterOutlet>
                 
