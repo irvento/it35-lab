e9eff0b413d14149e185161b562b968c3717724a
irvento <irvenabarquez@gmail.com>
2025-02-14 08:46:53 +0800

    Added home/details page route

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { 
+  IonButton,
   IonButtons,
     IonContent, 
     IonHeader, 
@@ -20,6 +21,9 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+      <IonButton routerLink="/it35-lab/app/home/details" expand="full">
+              Details
+          </IonButton>
       </IonContent>
     </IonPage>
   );
