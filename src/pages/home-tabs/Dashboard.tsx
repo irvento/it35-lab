16f5a3dedc1278e30f43d06046b2dffd0d9a02dd
irvn <129978135+irvento@users.noreply.github.com>
2025-03-06 22:38:12 +0800

    added some components to dashboard

--- src/pages/home-tabs/Dashboard.tsx
+++ src/pages/home-tabs/Dashboard.tsx
@@ -15,35 +15,78 @@ import {
       IonTitle, 
       IonToolbar 
   } from '@ionic/react';
+import { h } from 'ionicons/dist/types/stencil-public-runtime';
   
   const Dashboard: React.FC = () => {
     return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Dashboard</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
+      <IonHeader>
+        <IonToolbar color="primary">
+          <IonButtons slot='start'>
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Dashboard</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
         <IonGrid>
-        <IonRow>
-          <IonCol><IonCard>
-      <IonCardHeader>
-        <IonCardTitle>Card Title</IonCardTitle>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-      </IonCardHeader>
+          <IonRow>
+            <IonCol>
+              <IonCard color="tertiary" style={{ borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', height: '100%' }}>
+                <IonCardHeader style={{ textAlign: 'center', padding: '70px' }}>
+                  <IonCardTitle>REPORT A CRIME</IonCardTitle>
+                </IonCardHeader>
+        
+              </IonCard>
+            </IonCol>
 
-      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
-    </IonCard></IonCol>
-          <IonCol>2</IonCol>
-          <IonCol>3</IonCol>
-        </IonRow>
-      </IonGrid>
-        </IonContent>
-      </IonPage>
+            <IonCol>
+              <IonGrid>
+                <IonRow>
+
+                  <IonCol>
+                    <IonCard color="success" style={{ borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+                      <IonCardHeader>
+                        <IonCardTitle>Crimes</IonCardTitle>
+                        <IonCardSubtitle></IonCardSubtitle>
+                      </IonCardHeader>
+                      <IonCardContent>
+                        Stay safe.
+                      </IonCardContent>
+                    </IonCard>
+                  </IonCol>
+
+                  <IonCol>
+                    <IonCard color="warning" style={{ borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+                      <IonCardHeader>
+                        <IonCardTitle>Notifications</IonCardTitle>
+                        <IonCardSubtitle>Latest Updates</IonCardSubtitle>
+                      </IonCardHeader>
+                      <IonCardContent>
+                        Get notified about important changes and news.
+                      </IonCardContent>
+                    </IonCard>
+                  </IonCol>
+
+                </IonRow>
+              </IonGrid>
+            </IonCol>
+            
+          </IonRow>
+        </IonGrid>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonCard color="danger" style={{ borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', height: '100%' }}>
+                <IonCardHeader style={{ textAlign: 'center', padding: '70px' }}>
+                  <IonCardTitle>EMERGENCY HOTLINES</IonCardTitle>
+                </IonCardHeader>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
     );
   };
   
