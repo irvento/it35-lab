120525ca6d8b40adec52d0a4490e86c50aab3c77
irvn <129978135+irvento@users.noreply.github.com>
2025-03-07 22:10:19 +0800

    added content components to feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -16,69 +16,41 @@ import {
 } from '@ionic/react';
 
 const Feed: React.FC = () => {
-  // State to manage the list of items
-  const [items, setItems] = useState<string[]>([]);
+  const posts = [
+    { id: 1, user: 'Lawrence', content: 'Had a great day hiking today!', avatar: 'https://i.pravatar.cc/150?img=1' },
+    { id: 2, user: 'Acer', content: 'Just tried the best pizza ever!', avatar: 'https://scontent.fcgy1-3.fna.fbcdn.net/v/t39.30808-1/472908317_1942152872943037_6454809370600299567_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=101&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeGbFjGNoab4g_WdMy4BdTEnIe7711lYOosh7vvXWVg6i7nzj5_i2fsdi-zYP-UDOAMxv9TbzSqR39Vwf2cQKmqm&_nc_ohc=ATZFAcU0lKQQ7kNvgGzkJfn&_nc_oc=AdhPb0a__XTJBXZNq_-71pmCYkhkPdKATKgY2a2ukurgElLSGoi1olt5bbZi6RZ5LQc&_nc_zt=24&_nc_ht=scontent.fcgy1-3.fna&_nc_gid=ARV5HjiTxTnrST5UGpCYCfD&oh=00_AYEC5DhIazdkjJsh8DuPZSta3A6qa3Ghk5qMJM1C0TD1tg&oe=67D0D8E2' },
+    { id: 3, user: 'maJ', content: 'Loving the Ionic framework!', avatar: 'https://i.pravatar.cc/150?img=3' },
+    { id: 4, user: 'Joas', content: 'Just got a new puppy!', avatar: 'https://scontent.fcgy1-3.fna.fbcdn.net/v/t39.30808-1/464378467_512242568478472_6334836110421207698_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=101&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeHHdJX6vD8Hv40shYjUX2N2TpTcAARVRZVOlNwABFVFlZvglKJHEPMI7ns_qeOhjGMowU1cASMlerVKLg49-Ah5&_nc_ohc=16FVzH8KVN0Q7kNvgGItyu3&_nc_oc=AdjsG-aSH6sUuiJXYJlJ3wL3VfKouKRpel3CbngSyUDURMrxBr5E0hPqgkwD9qIbLeI&_nc_zt=24&_nc_ht=scontent.fcgy1-3.fna&_nc_gid=AS0jtHa1Q7wb1ERNtic5uCA&oh=00_AYGgu4JpGKi5pffbmKkWk1-WLRmCeFdXFnajo1ELCTfYag&oe=67D0B3DE' },
+];
 
-  // Load initial data
-  const loadData = () => {
-    const newItems = [];
-    for (let i = 0; i < 20; i++) {
-      newItems.push(`Item ${items.length + i + 1}`);
-    }
-    setItems([...items, ...newItems]);
-  };
-
-  // Load more data on infinite scroll
-  const onIonInfinite = (event: CustomEvent<void>) => {
-    setTimeout(() => {
-      loadData();
-      (event.target as HTMLIonInfiniteScrollElement).complete();
-
-     
-      if (items.length >= 100) {
-        (event.target as HTMLIonInfiniteScrollElement).disabled = true;
-      }
-    }, 1000); 
-  };
-
-  React.useEffect(() => {
-    loadData();
-  }, []);
-
-  return (
+return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>Feed</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonList>
-          {items.map((item, index) => (
-            <IonItem key={index}>
-              <IonAvatar slot="start" style={{ width: '120px', height: '120px' }}>
-                <img
-                  src={`https://picsum.photos/120/120?random=${index}`}
-                  alt="avatar"
-                  style={{ width: '100%', height: '100%' }}
-                />
-              </IonAvatar>
-              <IonLabel>{item}</IonLabel>
-            </IonItem>
-          ))}
-        </IonList>
-        <IonInfiniteScroll onIonInfinite={onIonInfinite}>
-          <IonInfiniteScrollContent
-            loadingText="Loading more data..."
-            loadingSpinner="bubbles"
-          />
-        </IonInfiniteScroll>
-      </IonContent>
+        <IonHeader>
+        <IonToolbar color="primary">
+
+                <IonTitle>Feed</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+
+        <IonContent fullscreen>
+            <IonList>
+                {posts.map(post => (
+                    <IonItem key={post.id}>
+                        <IonAvatar slot="start">
+                            <img src={post.avatar} alt={`${post.user}'s avatar`} />
+                        </IonAvatar>
+                        <IonLabel>
+                            <h2>{post.user}</h2>
+                            <p>{post.content}</p>
+                        </IonLabel>
+                    </IonItem>
+                ))}
+            </IonList>
+        </IonContent>
+
     </IonPage>
-  );
+);
+
 };
 
 export default Feed;
