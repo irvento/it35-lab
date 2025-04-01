f942f7868581c415c1fcd48f213547147e562eb4
irvn <129978135+irvento@users.noreply.github.com>
2025-04-01 23:13:27 +0800

    Updates

--- assets/ios.transition-legacy-bGbPLnvS.js
+++ assets/ios.transition-legacy-bGbPLnvS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bsg80rPW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BUnvloWy.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
