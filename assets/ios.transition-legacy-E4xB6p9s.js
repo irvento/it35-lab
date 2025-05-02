1b1397ff9adb5301d9400c09dd9e66d286c4c6b0
irvn <129978135+irvento@users.noreply.github.com>
2025-05-02 22:38:56 +0800

    Updates

--- assets/ios.transition-legacy-E4xB6p9s.js
+++ assets/ios.transition-legacy-E4xB6p9s.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CftbKqQL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-WAgmonJs.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
