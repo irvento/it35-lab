ad108d4249eaf5d7eb75e9dd4d35c40689b33e87
irvn <129978135+irvento@users.noreply.github.com>
2025-05-13 23:44:36 +0800

    Updates

--- assets/md.transition-legacy-DUjZ7QX-.js
+++ assets/md.transition-legacy-DUjZ7QX-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DITtuqhP.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Yj8wuxeS.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
