7c7af5f957c62dd6c4ff40aea17a8af9c07b0898
irvn <129978135+irvento@users.noreply.github.com>
2025-05-14 03:22:33 +0800

    Updates

--- assets/ios.transition-legacy-3cSDBhM6.js
+++ assets/ios.transition-legacy-3cSDBhM6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DU0BwrVn.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DESMWky6.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Yfc3oLvq.js
+++ assets/swipe-back-legacy-Yfc3oLvq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DU0BwrVn.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DESMWky6.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
